29b1607d962e989f511af85aa8d1f60949651178
paulobof <paulobof@gmail.com>
2023-10-15 18:50:11 -0300

    Resolvendo Exercicio Set Orderana (Aluno e Produto)

--- conhecendo-collection-java/src/set/Ordenacao/CadastroProdutos/CadastroProdutos.java
+++ conhecendo-collection-java/src/set/Ordenacao/CadastroProdutos/CadastroProdutos.java
@@ -1,4 +1,53 @@
 package set.Ordenacao.CadastroProdutos;
 
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
 public class CadastroProdutos {
+    //atributos
+    Set<Produto> cadastroProduto;
+
+    public CadastroProdutos() {
+        this.cadastroProduto = new HashSet<>();
+    }
+
+    public void adicionarProduto(long cod, String nome, double preco, int quantidade) {
+        cadastroProduto.add(new Produto(cod, nome, preco, quantidade));
+    }
+
+    public Set<Produto> exibirProdutosPorNome() {
+        if (!cadastroProduto.isEmpty()) {
+            Set<Produto> produtosPorNome = new TreeSet<>(cadastroProduto);
+            return produtosPorNome;
+        } else {
+            throw new RuntimeException("O set está vazio");
+        }
+    }
+
+    public Set<Produto> exibirProdutosPorPreco() {
+        if (!cadastroProduto.isEmpty()) {
+            Set<Produto> produtosPorPreco = new TreeSet<>(new ComparatorPorPreco());
+            produtosPorPreco.addAll(cadastroProduto);
+            return produtosPorPreco;
+        } else {
+            throw new RuntimeException("O set está vazio");
+        }
+    }
+
+    public static void main(String[] args) {
+        CadastroProdutos cadastroProdutos = new CadastroProdutos();
+
+        cadastroProdutos.adicionarProduto(1L, "Smartphone", 1000d, 10);
+        cadastroProdutos.adicionarProduto(2L, "Notebook", 1500d, 5);
+        cadastroProdutos.adicionarProduto(1L, "Mouse", 30d, 20);
+        cadastroProdutos.adicionarProduto(4L, "Teclado", 50d, 15);
+
+        System.out.println(cadastroProdutos.cadastroProduto);
+
+        System.out.println(cadastroProdutos.exibirProdutosPorNome());
+
+        System.out.println(cadastroProdutos.exibirProdutosPorPreco());
+    }
 }
---
+++ conhecendo-collection-java/src/set/Ordenacao/GerenciadorAlunos/Aluno.java
@@ -0,0 +1,60 @@
+package set.Ordenacao.GerenciadorAlunos;
+
+import java.util.Comparator;
+import java.util.Objects;
+
+public class Aluno implements Comparable<Aluno>{
+    //atributos
+    private String nome;
+    private long matricula;
+    private double media;
+
+    public Aluno(String nome, long matricula, double media) {
+        this.nome = nome;
+        this.matricula = matricula;
+        this.media = media;
+    }
+    public String getNome() {
+        return nome;
+    }
+
+    public long getMatricula() {
+        return matricula;
+    }
+
+    public double getMedia() {
+        return media;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Aluno aluno)) return false;
+        return getMatricula() == aluno.getMatricula();
+    }
+
+    @Override
+    public String toString() {
+        return "Aluno{" +
+                "nome='" + nome + '\'' +
+                ", matricula=" + matricula +
+                ", media=" + media +
+                '}';
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getMatricula());
+    }
+
+    @Override
+    public int compareTo(Aluno aluno) {
+        return nome.compareTo(aluno.getNome());
+    }
+}
+class ComparatorNota implements Comparator<Aluno> {
+    @Override
+    public int compare(Aluno o1, Aluno o2) {
+        return Double.compare(o1.getMedia(), o2.getMedia());
+    }
+}
